bb87525b543cc0f735aa72f5398a0ad9dc5ff0b1
Carlos Diaz <charliedn@gmail.com>
2022-11-19 11:44:31 +0000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Button colorScheme='blue' size="lg">Button</Button>
+  )
+}
